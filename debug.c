65985fc5874c1ed2810cec79e118690cd4b5ebd0
Alexis Philippon <alexphil@fu-r6-p8.s19.be>
2023-07-10 17:32:07 +0200

    Shortened debug functions purely for flexing

--- debug.c
+++ debug.c
@@ -6,7 +6,7 @@
 /*   By: alexphil <alexphil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 11:28:00 by alexphil          #+#    #+#             */
-/*   Updated: 2023/07/10 14:58:49 by alexphil         ###   ########.fr       */
+/*   Updated: 2023/07/10 17:30:26 by alexphil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,51 @@
 
 void	print_stack_a(t_stacks *stacks)
 {
-	t_node	*current;
-
 	if (!stacks->a->head)
 	{
 		printf("Stack A is empty.\n");
 		return ;
 	}
 	printf("Stack A:\n");
-	current = stacks->a->head;
-	while (1)
-	{
-		printf("%i < %i > %i",
-			current->prev->value, current->value, current->next->value);
-		if (current == stacks->a->head && current == stacks->a->tail)
-			printf("\tHEAD & TAIL\n");
-		else if (current == stacks->a->head)
-			printf("\tHEAD\n");
-		else if (current == stacks->a->tail)
-			printf("\tTAIL\n");
-		else
-			printf("\n");
-		if (current == stacks->a->tail)
-			break ;
-		current = current->next;
-	}
+	print_stack(stacks->a);
 }
 
 void	print_stack_b(t_stacks *stacks)
 {
-	t_node	*current;
-
 	if (!stacks->b->head)
 	{
 		printf("Stack B is empty.\n");
 		return ;
 	}
 	printf("Stack B:\n");
-	current = stacks->b->head;
-	while (1)
-	{
-		printf("%i < %i > %i",
-			current->prev->value, current->value, current->next->value);
-		if (current == stacks->b->head && current == stacks->b->tail)
-			printf("\tHEAD & TAIL\n");
-		else if (current == stacks->b->head)
-			printf("\tHEAD\n");
-		else if (current == stacks->b->tail)
-			printf("\tTAIL\n");
-		else
-			printf("\n");
-		if (current == stacks->b->tail)
-			break ;
-		current = current->next;
-	}
+	print_stack(stacks->b);
 }
 
 void	print_stacks(t_stacks *stacks)
 {
 	print_stack_a(stacks);
 	print_stack_b(stacks);
+}
+
+void	print_stack(t_stack	*stack)
+{
+	t_node	*current;
+	
+	current = stack->head;
+	while (1)
+	{
+		printf("%i < %i > %i",
+			current->prev->value, current->value, current->next->value);
+		if (current == stack->head && current == stack->tail)
+			printf("\tHEAD & TAIL\n");
+		else if (current == stack->head)
+			printf("\tHEAD\n");
+		else if (current == stack->tail)
+			printf("\tTAIL\n");
+		else
+			printf("\n");
+		if (current == stack->tail)
+			break ;
+		current = current->next;
+	}
 }
