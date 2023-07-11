bf53e40cbaefb6a0e0628ff86cb359d65849adad
Alexis Philippon <alexphil@fu-r6-p8.s19.be>
2023-07-11 11:41:17 +0200

    Reorganization

--- push_swap.c
+++ push_swap.c
@@ -6,117 +6,11 @@
 /*   By: alexphil <alexphil@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 13:43:08 by alexphil          #+#    #+#             */
-/*   Updated: 2023/07/11 11:01:59 by alexphil         ###   ########.fr       */
+/*   Updated: 2023/07/11 11:40:16 by alexphil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-int	ft_correct_str_input(char *s)
-{
-	int	i;
-
-	i = -1;
-	if (!s[0])
-		ft_errors(1);
-	while (s[++i])
-	{
-		if (!ft_is_digit(s[i]) && s[i] != ' ' && s[i] != '-')
-			ft_errors(1);
-		if (s[i] == '-' && !ft_is_digit(s[i + 1]))
-			ft_errors(1);
-		if (s[i] == '-' && i != 0 && s[i - 1] != ' ')
-			ft_errors(1);
-	}
-	return (1);
-}
-
-int	ft_count_numbers(char *s)
-{
-	int	x;
-
-	x = 0;
-	while (*s)
-	{
-		if (ft_is_digit(*s))
-			x++;
-		while (ft_is_digit(*s))
-			s++;
-		while (*s == ' ' || *s == '-')
-			s++;
-	}
-	return (x);
-}
-
-int	*ft_string_to_stack(char *s, int *size)
-{
-	char	**tmp;
-	int		i;
-	int		*stack;
-	int		overflow;
-
-	i = -1;
-	overflow = 0;
-	ft_correct_str_input(s);
-	*size = ft_count_numbers(s);
-	if (*size == 1)
-		ft_errors(0);
-	tmp = ft_split(s, ' ');
-	if (!tmp)
-		ft_errors(2);
-	stack = malloc(*size * sizeof(int));
-	if (!stack)
-		ft_errors(2);
-	while (++i < *size)
-		stack[i] = ft_atoi(tmp[i], &overflow);
-	free(tmp);
-	if (overflow)
-		ft_errors(1);
-	return (stack);
-}
-
-int	*ft_args_to_stack(int ac, char **av, int *size)
-{
-	int	i;
-	int	*stack;
-	int	overflow;
-
-	*size = ac - 1;
-	if (*size == 1)
-		ft_errors(0);
-	i = -1;
-	overflow = 0;
-	stack = malloc(*size * sizeof(int));
-	while (++i < *size)
-	{
-		if (!ft_correct_str_input(av[i + 1]))
-			ft_errors(1);
-		stack[i] = ft_atoi(av[i + 1], &overflow);
-	}
-	if (overflow)
-		ft_errors(1);
-	return (stack);
-}
-
-int	ft_has_doubles(int *stack, int size)
-{
-	int	i;
-	int	j;
-
-	i = -1;
-	while (++i < size)
-	{
-		j = -1;
-		while (++j < size)
-		{
-			if (j == i)
-				j++;
-			if (stack[i] == stack[j])
-				return (1);
-		}
-	}
-	return (0);
-}
 
 int	main(int ac, char **av)
 {
